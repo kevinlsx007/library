7ea1a0559c45a02739efa8c4e6b939956f499524
Sixing Li <kevinlsx007@hotmail.com>
2021-02-03 17:07:55 -0800

    Finished GUI backbone with html and css.

---
+++ main.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+// Book object constructor
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+// Function to add a new book
